50651c4cfacb1050d0f314791b9939987f750b7c
Alex-mp4 <alexander.heino@elev.ga.ntig.se>
2023-04-27 13:37:31 +0200

    setup

---
+++ src/Cardgame/executable.java
@@ -0,0 +1,12 @@
+package Cardgame;
+
+public class executable {
+    public static void main(String[] args) {
+        //new Thread(new Runnable() {
+          //  public void run() { ServerController.main(args); }
+        //}).start();
+        //new Thread(new Runnable() {
+          //  public void run() { ClientController.main(null); }
+        //}).start();
+    }
+}
